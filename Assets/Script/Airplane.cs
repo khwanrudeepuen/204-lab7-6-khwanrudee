59adc5f19bdaba65ae6c83593e6bb9b5b43f3989
khwanrudee <khwanrudee.pueb@bumail.net>
2025-03-03 10:41:12 +0700

    เคลื่อนที่ไปข้างหน้า

---
+++ Assets/Script/Airplane.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooter = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+        
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+    }
+}
